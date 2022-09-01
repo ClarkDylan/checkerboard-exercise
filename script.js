76ee4dc681628dd8f96ab1dfedaab92e9da151c4
ClarkDylan <clarkdylan00@gmail.com>
2022-08-31 20:10:43 -0500

    Random colors

--- script.js
+++ script.js
@@ -9,6 +9,27 @@ if (i % 2 !== 0){
   makeTile(red)
 }
 */
+// 16777215 is the number of possible colors in rgb (32 bit), and 16 in toString tells function to do radix or base 16 (hexidecimal)
+// function makeTile(color) {
+//   let body = document.getElementsByTagName('body');
+//   let tile = document.createElement('div');
+//   tile.style.width = '11.1%';
+//   tile.style.paddingBottom = '11.1%';
+//   tile.style.cssFloat = 'left';
+//   tile.style.backgroundColor = color;
+//   document.body.append(tile);
+// }
+
+// for (let i = 0; i < 81; i++) {
+//   if (i % 2 !== 0) {
+//     makeTile('red');
+//   } else if (i % 2 === 0) {
+//     makeTile('black');
+//   }
+// }
+
+// randomHexColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+// 16777215 is the number of possible colors in rgb (32 bit), and 16 in toString tells function to do radix or base 16 (hexidecimal)
 
 function makeTile(color) {
   let body = document.getElementsByTagName('body');
@@ -22,8 +43,8 @@ function makeTile(color) {
 
 for (let i = 0; i < 81; i++) {
   if (i % 2 !== 0) {
-    makeTile('red');
+    makeTile("#" + Math.floor(Math.random() * 16777215).toString(16));
   } else if (i % 2 === 0) {
-    makeTile('black');
+    makeTile("#" + Math.floor(Math.random() * 16777215).toString(16));
   }
 }
